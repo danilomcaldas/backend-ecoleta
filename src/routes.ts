1f041805c9aa46bdb11ff4ccf116751a2bbe7da2
Danilo Caldas <magalhaes.danilocaldas@gmail.com>
2020-06-30 18:43:39 -0300

    backend developed in the week NLW

---
+++ src/routes.ts
@@ -0,0 +1,19 @@
+import express, { request, response } from 'express';
+
+import PointsController from './controllers/pointsController';
+import ItemsController from './controllers/itemsController';
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itensController = new ItemsController();
+
+routes.get('/items',  itensController.index);
+
+routes.post('/points', pointsController.create);
+
+routes.get('/points/',  pointsController.index);
+routes.get('/points/:id',  pointsController.show);
+
+
+
+export default routes;
